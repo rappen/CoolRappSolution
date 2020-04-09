89849c5f6ab6459eba0e900c9193b2e2329e2325
Jonas Rapp <jonas@jonasr.app>
2020-04-09 18:00:27 +0200

    Add project files.

---
+++ CoolRappPlugin/RocketPlugin.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace CoolRappPlugin
+{
+    public class RocketPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = serviceFactory.CreateOrganizationService(context.InitiatingUserId);
+
+            var user = service.Retrieve("systemuser", context.InitiatingUserId, new ColumnSet("fullname"));
+            trace.Trace($"Hello {user["fullname"]}!");
+        }
+    }
+}
